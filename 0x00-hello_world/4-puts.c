d0f4695114219d3a6b21e5c510e69a10e1eafaf0
Emigham <emigham7@gmail.com>
2022-09-08 09:59:38 -0700

    c4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Serves as the entry point for the program
+ *
+ * Return: By defualt returns zero if no erros otherwise returns non-zero value.
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
