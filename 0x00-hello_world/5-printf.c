fb86c46a686559dd4bd4595ad96590fb89425586
Emigham <emigham7@gmail.com>
2022-09-08 13:38:10 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * this program prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
-int main(0)
+int main(void)
 {
 	printf("with proper grammer,but outcome is a piece of art,\n");
 	return (0);
