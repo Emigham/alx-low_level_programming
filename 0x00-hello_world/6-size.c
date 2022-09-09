eaf1c6b7df8b45735f6f01e3f7543a990cd5194b
Emigham <emigham7@gmail.com>
2022-09-08 17:01:17 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main print the string inthe put function
+ * main - print the string inthe put function
  *
  * Description: using the main function
  * this program prints "programming is like building a multilingual puzzle
@@ -21,6 +21,3 @@ int main(void)
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
-
-
-
