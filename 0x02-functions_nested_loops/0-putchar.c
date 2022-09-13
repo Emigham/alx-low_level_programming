6f3058e4700630476b3e9a340517b0430514c2c0
Emigham <emigham7@gmail.com>
2022-09-13 08:54:42 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,8 @@
  * Return: Always 0
  */
 
-int main(void)
+main()
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	printf("_putchar\n");
 	return (0);
 }
