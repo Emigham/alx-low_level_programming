576c52c87faac9e18cb463bbedf3db56b3a591bf
Emigham <emigham7@gmail.com>
2022-09-13 04:28:49 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *myVar = "alx";
+
+	while (*myVar)
+	{
+		_putchar (*myVar);
+		myVar++;
+	}
+	_putchar ("\n");
+
+	return (0);
+}
