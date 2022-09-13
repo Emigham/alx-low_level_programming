f75896d07172893c8b04e5911ff422788c12247c
Emigham <emigham7@gmail.com>
2022-09-13 06:57:16 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	char *myVar = "alx";
-
-	while (*myVar)
-	{
-		_putchar (*myVar);
-		myVar++;
-	}
-	_putchar ("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar("\n");
 
 	return (0);
 }
