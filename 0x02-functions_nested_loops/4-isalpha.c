2c844a9bcc73b22401d00e21fc1466f3bfdbd409
Emigham <emigham7@gmail.com>
2022-09-14 14:08:58 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,14 +10,9 @@
  */
 int _isalpha(int c)
 {
-	int i = isalpha(int c);
-
-	if (i > 0)
-	{
+	if ((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
