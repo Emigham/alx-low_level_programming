ccf2382c72e37bc1eee9038c192894d2d6b9a3a0
Emigham <emigham7@gmail.com>
2022-09-19 07:18:12 -0700

    m

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,5 +17,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
+int _strlen(char *s);
+void rev_string(char *s);
 
 #endif /*MAIN.H*/
